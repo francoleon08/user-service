ec4ca98a9810e11eb33e682ee1861a9192d397bd
Franco León <89669737+francoleon08@users.noreply.github.com>
2024-08-05 17:24:21 -0300

    Update CORS main.py

--- src/main.py
+++ src/main.py
@@ -4,6 +4,14 @@ from .controller import routes
 app = FastAPI()
 app.include_router(routes.app)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/users/")
 def root():
